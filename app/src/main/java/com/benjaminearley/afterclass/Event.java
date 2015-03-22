7b771ac1534f2322f48c699ce44ae1bfe993d070
John Spaetzel <john.spaetzel@gmail.com>
2015-03-21 23:30:55 -0400

    Added Code for Calling Events Via API

---
+++ app/src/main/java/com/benjaminearley/afterclass/Event.java
@@ -0,0 +1,12 @@
+package com.benjaminearley.afterclass;
+
+public class Event {
+    public int Id;
+    public String Name;
+    public String Description;
+    public String StartDatetime;
+    public String EndDatetime;
+    public String LocationName;
+    public String Latitude;
+    public String Longitude;
+}
--- app/src/main/java/com/benjaminearley/afterclass/HomeActivity.java
+++ app/src/main/java/com/benjaminearley/afterclass/HomeActivity.java
@@ -4,9 +4,16 @@ import android.content.Intent;
 import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
@@ -15,6 +22,9 @@ import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class HomeActivity extends ActionBarActivity implements
@@ -29,6 +39,7 @@ public class HomeActivity extends ActionBarActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getEvents();
         setContentView(R.layout.activity_home_screen);
 
     }
@@ -109,4 +120,66 @@ public class HomeActivity extends ActionBarActivity implements
                 mGoogleApiClient, mLocationRequest, this);
     }
 
+    private void getEvents() {
+
+        String url = "http://afterclass.johnspaetzel.com/events";
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("JSON Request", response.toString());
+                int _size;
+                try {
+                    // Parsing json object response
+
+                    JSONObject events = response.getJSONObject("events");
+
+                    Log.d("Json Response", events.toString());
+                    /*int numberOfPhotos = JSONPhotos.getInt("total");
+
+                    if (numberOfPhotos > 9)
+                        _size = 9;
+                    else
+                        _size = numberOfPhotos;
+
+                    JSONArray JSONPhotoList = JSONPhotos.getJSONArray("photo");
+
+                    for (int i = 0; i < _size; i++) {
+                        Photo photo = new Photo();
+
+                        JSONObject JSONPhoto = (JSONObject) JSONPhotoList.get(i);
+                        photo.id = JSONPhoto.getString("id");
+                        photo.owner = JSONPhoto.getString("owner");
+                        photo.secret = JSONPhoto.getString("secret");
+                        photo.server = JSONPhoto.getString("server");
+                        photo.farm = JSONPhoto.getString("farm");
+                        photos.add(photo);
+
+
+                    }
+                    //add photos to slide
+                    spinner.setVisibility(View.GONE);
+                    setFlickrSlideshowPhotos();*/
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Volley Error", "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
+    }
 }
